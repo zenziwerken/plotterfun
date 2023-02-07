f2e9a5459463eb14b4b0305c36b231c6b81f8423
Daniel Groß <daniel@zenziwerken.de>
2023-02-07 10:40:38 +0100

    Update subline.js

--- subline.js
+++ subline.js
@@ -14,43 +14,44 @@ onmessage = function(e) {
   const getPixel = pixelProcessor(config, pixData)
 
   const height = config.height - 1;
+  const width  = config.width;
 
   const lineCount = config['Line Count'];
   const sublines  = config['Sublines'];
   const direction = config.Direction;
   const amplitude = config.Amplitude / sublines / lineCount;
-  
-
   const precision = 6 - config.Sampling;
 
   let squiggleData = [];
-
   let lastr = -1;
-  
-  if (direction == 'Vertical') {
 
+  switch (direction) {
+
+    case 'Vertical': {
     const incr_x    = Math.floor(height / lineCount);
     const incr_y    = config.Sampling;
-    for (let x = 0; x <= config.width; x += incr_x) {
+    for (let x = 0; x <= width; x += incr_x) {
+      let r_sum = 0;
       for (let j = 0; j < sublines; j++) {
         let line = [];
-        let r_sum = 0;
-  
-        line.push([x + (amplitude * j * getPixel(x,0)),0])
-  
+        let z = getPixel(x, 0)
+        let r = amplitude * j * z;
+        line.push([x + r,0]);
         for (let y = 0; y <= height; y += incr_y) {
-          let z = getPixel(x, y)
-          let r = amplitude * j * z;
-          if (r.toFixed(precision) != lastr.toFixed(precision)) {
+          z = getPixel(x, y)
+          r = amplitude * j * z;
+          if (r.toFixed(precision) != lastr.toFixed(precision) ) {
             line.push([x + r, y]);
             lastr = r;
           }
           r_sum += r;
         } 
-        line.push([x + (amplitude * j * getPixel(x, height)), height]);
-  
-        if (r_sum > 1) {
-          line.push([x - (amplitude * j * getPixel(x, height)), height])
+        z = getPixel(x, height)
+        r = amplitude * j * z;
+        //if (j == 0) {line.push([x + r, height + 10]);} else {line.push([x + r, height]);}
+        line.push([x + r, height]);
+        if (r_sum > 10) {
+          line.push([x - r, height])
           for (let y = height; y > 0 ; y -= incr_y) {
             let z = getPixel(x, y)
             let r = amplitude * j * z;
@@ -58,24 +59,24 @@ onmessage = function(e) {
               line.push([x - r, y]);
               lastr = r;
             }
-          } 
-          line.push([x - (amplitude * j * getPixel(x,0)),0])
+          }
+          z = getPixel(x, 0)
+          r = amplitude * j * z;
+          line.push([x - r,0])
         }
-        
         squiggleData.push(line)
       }
     }
-  } else {
+  } break;
+  default: //Horizontal
     const incr_x    = config.Sampling;
-    const incr_y    = Math.floor(config.width / lineCount);
-
+    const incr_y    = Math.floor(width / lineCount);
     for (let y = 0; y <= height; y += incr_y) {
+      let r_sum = 0;
       for (let j = 0; j < sublines; j++) {
         let line = [];
-        let r_sum = 0;
-  
         line.push([0, y + (amplitude * j * getPixel(0, y))])
-        for (let x = 0; x < config.width; x += incr_x) {
+        for (let x = 0; x < width; x += incr_x) {
           let z = getPixel(x, y)
           let r = amplitude * j * z;
           if (r.toFixed(precision) != lastr.toFixed(precision)) {
@@ -84,11 +85,10 @@ onmessage = function(e) {
           }
           r_sum += r;
         } 
-        line.push([config.width, y + (amplitude * j * getPixel(config.width, y))]);
-  
-        if (r_sum > 1) {
-          line.push([config.width, y - (amplitude * j * getPixel(config.width, y))])
-          for (let x = config.width; x >=0 ; x -= incr_x) {
+        line.push([width, y + (amplitude * j * getPixel(width, y))]);
+        if (r_sum > 10) {
+          line.push([width, y - (amplitude * j * getPixel(width, y))])
+          for (let x = width; x >=0 ; x -= incr_x) {
             let z = getPixel(x, y)
             let r = amplitude * j * z;
             if (r.toFixed(precision) != lastr.toFixed(precision)) {
@@ -98,12 +98,11 @@ onmessage = function(e) {
           } 
           line.push([0, y - (amplitude * j * getPixel(0, y))])
         }
-  
         squiggleData.push(line)
       }
     }
-}
-//if (squiggleData > 0) 
-postLines(squiggleData);
+  }
+  if (squiggleData.length>0)
+    postLines(squiggleData);
 }
 
