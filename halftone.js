f6fdc26786ae17cf2b00313fdf6cf7527f05f0e9
Daniel Groß <daniel@zenziwerken.de>
2023-02-08 15:19:26 +0100

    Update halftone.js

--- halftone.js
+++ halftone.js
@@ -3,9 +3,10 @@ importScripts('helpers.js', 'external/stackblur.min.js')
 postMessage(['sliders', defaultControls.concat([
   { label: 'Divisions', value: 25, min: 10, max: 100 },
   { label: 'Factor', value: 100, min: 10, max: 400 },
-  { label: 'Cutoff', value: 1, min: 0, max: 254},
+  { label: 'Cutoff', value: 1, min: 1, max: 254 },
   { label: 'Interlaced', type: 'checkbox' },
-  { label: 'Type', type:'select', options:['Circles', 'Diamond', 'Squares', 'Spirals']},
+  { label: 'Filled', type: 'checkbox' },
+  { label: 'Type', type:'select', options:['Circles', 'Diamond', 'Squares']},
 ])]);
 
 
@@ -14,6 +15,7 @@ onmessage = function (e) {
 
   const major = (config.width + config.height) / config.Divisions / 2;
   const interlaced = config.Interlaced;
+  const filled = config.Filled;
   const factor = config.Factor;
   const type = config.Type;
 
@@ -34,49 +36,54 @@ onmessage = function (e) {
       let z = getPixel(x, y);
       if (z < config.Cutoff) continue;
       let radius = z * hm / 255 * factor / 100;
+
       switch (type) {
 
-      case 'Squares': {
-        for (r = radius; r >= 0; r--) {
-          let cube = [ [x - r,y - r], [x + r,y - r], [x + r, y + r], [x - r, y + r] ];
-          polygons.push(cube);
-        }
-      } break;
+        case 'Squares': {
+          let r_end = (filled) ? 0 : radius;
+          for (r = radius; r >= r_end; r--) {
+            let cube = [ [x - r,y - r], [x + r,y - r], [x + r, y + r], [x - r, y + r] ];
+            polygons.push(cube);
+          }
+        } break;
 
-      case 'Diamond': {
-        let r = radius;
-        let diamond = [ [x,y + r], [x + r,y], [x, y - r], [x - r,y] ];
-        polygons.push(diamond);
-      } break;
+        case 'Diamond': {
+          let r = radius;
+          let r_end = (filled) ? 0 : radius;
+          for (r = radius; r >= r_end; r--) {
+            let diamond = [ [x,y + r], [x + r,y], [x, y - r], [x - r,y] ];
+            polygons.push(diamond);
+          }
+        } break;
 
-      // adapted from stipple.js
-      case 'Spirals': {
-        let theta=0, spiral=[]
-        if (radius>=0.2) {
-        while (radius>=0.1) {
-          spiral.push( [(x - hm / 2) + radius*Math.cos(theta), (y - hm / 2) + radius*Math.sin(theta)] )
-          theta+=0.5
-          if (theta>6.3) radius-=0.1 //do one full loop before spiraling in
+        default: { //Circles
+          if (filled) {
+            // adapted from stipple.js
+            let theta = 0, spiral = []
+            if (radius >= 0.2) {
+              while (radius >= 0.1) {
+                spiral.push( [(x - hm / 2) + radius * Math.cos(theta), (y - hm / 2) + radius * Math.sin(theta)] )
+                theta += 0.5
+                if (theta > 6.3) radius -= 0.1 //do one full loop before spiraling in
+              }
+            }
+            if (spiral.length > 0)
+              lines.push(spiral);
+          } else {
+            circles.push({ x: x - hm / 2, y: y - hm / 2, r: radius });
+          }
         }
       }
-      if (spiral.length>0)
-      lines.push(spiral)
-      } break;
-
-      default: //Circles
-        circles.push({ x: x - hm / 2, y: y - hm / 2, r: radius });
-      }
-      
     }
   }
 
-  if (circles.length>0)
+  if (circles.length > 0)
     postCircles(circles);
 
-  if (lines.length>0)
+  if (lines.length > 0)
     postLines(lines);
 
-  if (polygons.length>0)
+  if (polygons.length > 0)
     postPolygons(polygons);
 }
 
